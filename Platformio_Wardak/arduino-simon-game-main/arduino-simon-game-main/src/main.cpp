2a0498b0d0c2afc3eecd3fd3334157027f08cb84
sami313-bit <war66222@gmail.com>
2025-11-20 17:36:34 +0100

    fizz buzz uplaud

--- Platformio_Wardak/arduino-simon-game-main/arduino-simon-game-main/src/main.cpp
+++ Platformio_Wardak/arduino-simon-game-main/arduino-simon-game-main/src/main.cpp
@@ -1,11 +1,37 @@
 #include <Arduino.h>
 
+int conuter = 0;
+
+
 void setup() 
 {
   Serial.begin(9600);
 }
 void loop()
 {
-  Serial.println("abc");
+  conuter++;
+  if(conuter %3 == 0)
+  {
+    if (conuter % 5 == 0)
+    {
+      Serial.println(" fizz Buzz");
+    }
+    else{
+      Serial.println("Buzz");
+    }
+
+  }
+  else{
+    if (conuter %5 ==0)
+    {
+      Serial.println("Buzz");
+    }
+    else{
+      Serial.println(conuter);
+    }
+    
+
+  }
+  
   delay(500);
 }
