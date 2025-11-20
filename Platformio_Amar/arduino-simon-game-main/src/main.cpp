96910e24eaac39b9383cf13696d8c42efef3cd36
Amar092007 <amar.pajalic@ms.bulme.at>
2025-11-20 17:37:11 +0100

    Buzz Fizz programm

--- Platformio_Amar/arduino-simon-game-main/src/main.cpp
+++ Platformio_Amar/arduino-simon-game-main/src/main.cpp
@@ -1,8 +1,30 @@
 #include <Arduino.h>
+
+int counter = 0;
+
 void setup() {
   Serial.begin(9600);
 }
 void loop() {
-  Serial.println("abc");
+  counter++;
+
+if (counter % 3 == 0){
+  if(counter % 5 == 0){
+    Serial.println("Fizz Buzz");
+  }
+  else{
+    Serial.println("Fizz");
+  }
+}
+else{
+  if(counter%5==0){
+    Serial.println("Buzz");
+  }
+  else{
+    Serial.println(counter);
+  }
+}
+
+
   delay(500);
 }
