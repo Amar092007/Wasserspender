09c694e46bd96c854a9830d4536f67c6e81d2293
Amar092007 <amar.pajalic@ms.bulme.at>
2025-12-04 15:41:33 +0100

    Added two buttons, added two leds

--- Platformio_Amar/arduino-simon-game-main/src/main.cpp
+++ Platformio_Amar/arduino-simon-game-main/src/main.cpp
@@ -1,30 +1,43 @@
 #include <Arduino.h>
+#include <Bounce2.h>
+
+//this is the main file
+
+#define BTN_YLW 2
+#define BTN_BLU 3
+
+#define LED_GRN 8
+#define LED_PKN 9
+
+Bounce2 :: Button btnYellow = Bounce2::Button();
+Bounce2 :: Button btnBlue = Bounce2::Button();;
 
 int counter = 0;
 
-void setup() {
+
+void setup () {
   Serial.begin(9600);
-}
-void loop() {
-  counter++;
+  pinMode(LED_GRN, OUTPUT);
+  pinMode(LED_PKN, OUTPUT);
 
-if (counter % 3 == 0){
-  if(counter % 5 == 0){
-    Serial.println("Fizz Buzz");
-  }
-  else{
-    Serial.println("Fizz");
-  }
-}
-else{
-  if(counter%5==0){
-    Serial.println("Buzz");
-  }
-  else{
-    Serial.println(counter);
-  }
-}
+  btnYellow.attach(BTN_YLW,INPUT_PULLUP);
+  btnYellow.setPressedState(LOW);
 
+  btnBlue.attach(BTN_BLU,INPUT_PULLUP);
+  btnBlue.setPressedState(LOW);
+}
+void loop () {
+  btnYellow.update();
+  btnBlue.update();
 
-  delay(500);
+  if(btnYellow.pressed()){
+    Serial.println("Yellow pressed");
+    digitalWrite(LED_GRN,HIGH);
+    delay(2000);
+    digitalWrite(LED_GRN,LOW);
+  }
+
+  if(btnBlue.pressed()){
+    Serial.println("Blue pressed");
+  }
 }
